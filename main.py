3d4c0840df4d21f4bf0b290e3d578750e310a887
Alenkq <37805183+Alen-QK@users.noreply.github.com>
2023-02-20 19:39:52 -0800

    init working counter

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from modules.comic_main_page import comic_main_page
 from modules.chapter_comic_page import chapter_comic_page
 
 session = requests.Session()
-target = 'TbCLIzv0'
+target = '2X_-40f-'
 chapters_array = comic_main_page(target, session)
+count = 0
+total = len(chapters_array)
 
 for chapter in chapters_array:
     chapter_comic_page(chapter)
+    count += 1
+    print('Complete %d chapters, %d is pending\n' % (count, total - count))
 
 print('done')
