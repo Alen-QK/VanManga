811407926e0673da3e2479ec902a8ead30305413
Alen <solaire_sogo@yahoo.com>
2024-10-29 19:27:05 -0700

    add lib pagination & thumbnail api

---
+++ utils/save_thumbnail.py
@@ -0,0 +1,16 @@
+import base64
+
+from utils.make_path import path_exists_make
+
+
+def saveThumbnail(manga_object):
+    manga_id, thumbnail = manga_object["manga_id"], manga_object["thumbnail"]
+    # path_exists_make('/vanmanga/thumbnails')
+    # thumbnail_path = f'/vanmanga/thumbnails/{manga_id}.jpg'
+    path_exists_make('../thumbnails')
+    thumbnail_path = f'../thumbnails/{manga_id}.jpg'
+
+    with open(thumbnail_path, 'wb') as f:
+        f.write(base64.b64decode(thumbnail))
+
+    return f'{manga_id}.jpg'
